ed88dc1cf79578ae232de280af614b63d08fb76f
Dylan Raye-Leonard <drayeleonard@gmail.com>
2022-05-23 19:16:19 -0600

    commit tree 17afc87aeef3cb9b307beaca1372e0c221a3f036

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,20 +1,34 @@
-ENV['RACK_ENV'] = 'test'
+ENV["RACK_ENV"] = "test"
 require_relative "../config/environment"
 require "sinatra/activerecord/rake"
 
 RSpec.configure do |config|
   # Database setup
 
+  ########## begin commented out text ##########
+
   config.before(:suite) do
     DatabaseCleaner.strategy = :transaction
     DatabaseCleaner.clean_with(:truncation)
   end
 
-  config.around do |example|
-    DatabaseCleaner.cleaning do
-      example.run
-    end
-  end
+  config.around { |example| DatabaseCleaner.cleaning { example.run } }
+
+  ########## end commented out text ##########
+
+  ########## begin insert ##########
+
+  # config.before(:suite) { DatabaseCleaner.clean_with(:truncation) }
+
+  # config.before { DatabaseCleaner.strategy = :transaction }
+
+  # config.before(:each, js: true) { DatabaseCleaner.strategy = :truncation }
+
+  # config.before { DatabaseCleaner.start }
+
+  # config.after { DatabaseCleaner.clean }
+
+  ########## end insert ##########
 
   config.expect_with :rspec do |expectations|
     expectations.include_chain_clauses_in_custom_matcher_descriptions = true
@@ -23,15 +37,24 @@ RSpec.configure do |config|
   config.mock_with :rspec do |mocks|
     mocks.verify_partial_doubles = true
   end
-  
+
   config.shared_context_metadata_behavior = :apply_to_host_groups
 end
 
 def migrate!(direction, version)
   migrations_paths = ActiveRecord::Migrator.migrations_paths
-  migrations = ActiveRecord::MigrationContext.new(migrations_paths, ActiveRecord::SchemaMigration).migrations
-  
+  migrations =
+    ActiveRecord::MigrationContext.new(
+      migrations_paths,
+      ActiveRecord::SchemaMigration
+    ).migrations
+
   ActiveRecord::Migration.suppress_messages do
-    ActiveRecord::Migrator.new(direction, migrations, ActiveRecord::SchemaMigration, version).migrate
+    ActiveRecord::Migrator.new(
+      direction,
+      migrations,
+      ActiveRecord::SchemaMigration,
+      version
+    ).migrate
   end
 end
